bd5ff6b77a693ccfc9c5948bc67997aed347820c
Hamza Kadiri <hamza.kadiri@student.ecp.fr>
2017-11-17 01:59:35 +0100

    updated constructors for BoxRoom & ShockRoom

--- src/BoxRoom.java
+++ src/BoxRoom.java
@@ -1,10 +1,13 @@
+import others.IDGenerator;
 
 public class BoxRoom extends Room {
 	public BoxRoom() {
+		this.ID = IDGenerator.getInstance().getNextID();
 	}
 	
 	public BoxRoom(String name) {
 		this.name=name;
+		this.ID = IDGenerator.getInstance().getNextID();
 		
 	}
 
--- src/ShockRoom.java
+++ src/ShockRoom.java
@@ -1,10 +1,13 @@
+import others.IDGenerator;
 
 public class ShockRoom extends Room {
 	public ShockRoom() {
+		this.ID = IDGenerator.getInstance().getNextID();
 		
 	}
 public ShockRoom(String name) {
 	this.name=name;
+	this.ID = IDGenerator.getInstance().getNextID();
 		
 	}
 
