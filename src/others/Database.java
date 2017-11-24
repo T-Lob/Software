ca810c5aa0a71c7b15a703b9780673d67ecd426f
T-Lob <wassim.ouakrim@student.ecp.fr>
2017-11-24 02:07:23 +0100

    Database actualisée

--- src/others/Database.java
+++ src/others/Database.java
@@ -6,22 +6,20 @@ import java.util.ArrayList;
 public class Database {
 	private static  ArrayList<ED> generatedEDs = new ArrayList<ED>();
 
-public static ArrayList<ED> getGeneratedEDs() {
-	return generatedEDs;
-}
-public static void addToGeneratedEDs(ED ed) {
-	Database.generatedEDs.add(ed);
-}
-
-
-public static ED getEDbyName(String EDname) {
-	for (ED ed:generatedEDs) {
-		if (ed.getEDname().equalsIgnoreCase(EDname)) {
-				return ed;
-		}
-	
+	public static ArrayList<ED> getGeneratedEDs() {
+		return generatedEDs;
 	}
-	return null;
 	
-}
+	public static void addToGeneratedEDs(ED ed) {
+		Database.generatedEDs.add(ed);
+	}
+
+	public static ED getEDbyName(String edName) {
+		for (ED ed:generatedEDs) {
+			if (ed.getEDname().equalsIgnoreCase(edName)) {
+				return ed;
+			}
+		}
+		return null;
+	}
 }
